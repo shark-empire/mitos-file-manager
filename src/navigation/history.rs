ba87bcb3bdd144396cb052665cb039d13775b6dd
Shark-empire <babor3331@icloud.com>
2026-08-27 16:32:34 +0000

    Implement back and forward navigation in History
    Added back_stack and forward_stack to manage navigation history.

--- src/navigation/history.rs
+++ src/navigation/history.rs
@@ -3,6 +3,8 @@ use std::path::PathBuf;
 #[derive(Clone, Default)]
 pub struct History {
     stack: Vec<PathBuf>,
+    back_stack: Vec<PathBuf>,
+    forward_stack: Vec<PathBuf>,
 }
 
 impl History {
@@ -20,5 +22,25 @@ impl History {
 
     pub fn clear(&mut self) {
         self.stack.clear();
+    }
+
+    
+
+        pub fn go_back(&mut self, current: &PathBuf) -> Option<PathBuf> {
+        if let Some(prev) = self.back_stack.pop() {
+            self.forward_stack.push(current.clone());
+            Some(prev)
+        } else {
+            None
+        }
+    }
+
+        pub fn go_forward(&mut self, current: &PathBuf) -> Option<PathBuf> {
+        if let Some(next) = self.forward_stack.pop() {
+            self.back_stack.push(current.clone());
+            Some(next)
+        } else {
+            None
+        }
     }
 }
