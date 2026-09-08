05a3bafacc04b3ac8c2a9c3370abe816c1c917ba
Shark-empire <babor3331@icloud.com>
2026-09-08 15:43:09 +0000

    Add file row rendering function in file_list.rs

---
+++ src/ui/file_list.rs
@@ -0,0 +1,14 @@
+use mitos_utils::common::permissions::format_permissions;
+use gtk::prelude::*;
+
+fn render_file_row(file: &FileEntry) -> gtk::Widget {
+    let row = gtk::Box::new(gtk::Orientation::Horizontal, 8);
+    
+    // Reuse the exact formatting logic from `mitos-ls`
+    let perms = format_permissions(file.mode); 
+    
+    row.append(&gtk::Label::new(Some(&perms)));
+    row.append(&gtk::Label::new(Some(&file.name)));
+    
+    row.upcast()
+}
