e0a168dee95a1da3810e4a919b6004bc02decdf8
Shark-empire <babor3331@icloud.com>
2026-09-18 20:50:41 +0000

    Change SaveFiles to SaveFile in PortalRequest

--- src/main.rs
+++ src/main.rs
@@ -2768,7 +2768,7 @@ fn build_ui(
                             },
                         );
                     }
-                    portal::service::PortalRequest::SaveFiles {
+                    portal::service::PortalRequest::SaveFile {
                         title,
                         current_folder,
                         files,
